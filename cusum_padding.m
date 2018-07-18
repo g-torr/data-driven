f3adda1ee838e39c94cbe08f9a752689c6780ce3
gtorrisi <giuseppe.torrisi@ens-lyon.fr>
2018-07-18 09:22:29 +0100

    padding

---
+++ cusum_padding.m
@@ -0,0 +1,13 @@
+function [cusum,k] = my_cusum(oxy)
+%this function gives the cusum and the index of the changepoint 
+N=length(oxy(:,1))
+t1=1:N;
+cusum_1st=sqrt((N-t1)./(N*t1))'.*cumsum(oxy(1:end,:));
+t2=2:N;
+cusum_2nd=flipud(cumsum(flipud(oxy)));% the second term in cusum formula starts from t+1
+cusum_2nd=cusum_2nd(2:end,:)*sqrt(t2/((N-t2)*N));
+cusum=cusum_1st+padarray(cusum_2nd,1,"pre");
+
+[M,k]=max(diag(cusum*inv(cov(oxy))*cusum'))
+end
+
