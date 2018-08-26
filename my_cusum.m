af10c373f9e61cc8efd6d7c636067174dcf97e6d
gtorrisi <giuseppe.torrisi@ens-lyon.fr>
2018-08-26 23:51:37 +0100

    multiple changepoints

--- my_cusum.m
+++ my_cusum.m
@@ -5,8 +5,8 @@ cusum_2nd=flipud(cumsum(flipud(oxy)));
 t1=1:N-1;
 cusum_1st=sqrt((N-1-t1)./((N-1)*t1))'.*cumsum(oxy(1:end-1,:));
 t2=1:N-1;
-cusum_2nd=cusum_2nd(2:end,:)*sqrt(t2/((N-t2)*N));
-cusum=cusum_1st+cusum_2nd;
+cusum_2nd=cusum_2nd(2:end,:)*sqrt(t2./((N-t2).*N));
+cusum=cusum_1st-cusum_2nd;
 
 [M,k]=max(diag(cusum/(cov(oxy(1:end-1,:)))*cusum'))
 end
