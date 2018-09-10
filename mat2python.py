a2a2df27bb0e497c538f019e6e2258a6e870c19d
peppe92 <pepponetorrisi@hotmail.it>
2018-09-10 18:24:06 +0100

    first commit averaging

--- mat2python.py
+++ mat2python.py
@@ -1,7 +1,7 @@
 import scipy
 from scipy import io
-def mat2python(repetition_number=0):
+def mat2python(patient=1,repetition_number=0):
 	'''This function read the SO1.mat file. The experiment is repeated 3 times, repetition_number can take values 0,1,2'''
 	print(" I am loading the "+str(repetition_number)+" repetition of the protocol")
-	oxy,fs,trial,y,classes=scipy.io.loadmat("S01.mat")["data"][0][repetition_number][0,0]
+	oxy,fs,trial,y,classes=scipy.io.loadmat("./data/S0"+str(patient)+".mat")["data"][0][repetition_number][0,0]
 	return oxy,trial[:,0],classes
